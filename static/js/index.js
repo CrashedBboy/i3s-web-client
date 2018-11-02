7b24cf942aaf6f26f20895755413347bf6b70adb
CrashedBboy <crashedbboy@gmail.com>
2018-11-02 10:23:04 +0800

    add debugging control option

--- static/js/index.js
+++ static/js/index.js
@@ -6,7 +6,9 @@ let showingBuildings = new Array();
 let camerInfoShowed = false;
 let displayTimestamp;
 
-let DEBUG = true;
+const DEBUG = true;
+const SHOW_MBS = false;
+const SHOW_BUILDING = true;
 
 $(document).ready(function () {
     viewer = new Cesium.Viewer("cesium-container", {
@@ -218,120 +220,123 @@ function processNode(node) {
 
     let color = getLevelColor(node);
 
-    let mbs = viewer.entities.add({
-        position: Cesium.Cartesian3.fromDegrees(node.mbs[0], node.mbs[1], node.mbs[2]),
-        ellipsoid: {
-            radii: new Cesium.Cartesian3(node.mbs[3], node.mbs[3], node.mbs[3]),
-            material: color
-        }
-    });
+    if (SHOW_MBS) {
+        let mbs = viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(node.mbs[0], node.mbs[1], node.mbs[2]),
+            ellipsoid: {
+                radii: new Cesium.Cartesian3(node.mbs[3], node.mbs[3], node.mbs[3]),
+                material: color
+            }
+        });
 
-    showingMBS.push(mbs);
-
-    if (buildingShowed(node)) {
-        return;
+        showingMBS.push(mbs);
     }
 
-    $.ajax({
-        url: layerUrl + '/nodes/' + node.id + '/' + node.featureData[0].href,
-        type: 'GET',
-        dataType: 'json'
-    }).done(function (featureData) {
 
+    if (SHOW_BUILDING) {
+        if (buildingShowed(node)) {
+            return;
+        }
+    
         $.ajax({
-            url: layerUrl + '/nodes/' + node.id + '/' + node.geometryData[0].href,
-            xhrFields: {
-                responseType: "arraybuffer"
-            },
-            processData: false
-        }).done(function (geometryBuffer) {
-            console.log(featureData);
-            console.log(geometryBuffer);
-
-            let features = featureData.featureData;
-            let geometryData = featureData.geometryData[0];
-            let vertexAttributes = geometryData.params.vertexAttributes;
-            let instances = new Array();
-
-            let vertexPerFeature = 3;
-            if (geometryData.params.type == 'triangles') {
-                vertexPerFeature = 3;
-            } else if (geometryData.params.type == 'lines') {
-                vertexPerFeature = 2;
-            } else if (geometryData.params.type == 'points') {
-                vertexPerFeature = 1;
-            }
-
-            for (let i = 0; i < features.length; i++) {
-                let feature = features[i];
-                let faceRange = feature.geometries[0].params.faceRange; // faceRange is the index of faces(triangles): [first, last]
-                let featureVertices = new Float32Array(
-                    geometryBuffer,
-                    vertexAttributes.position.byteOffset + faceRange[0] * (vertexPerFeature * vertexAttributes.position.valuesPerElement) * Float32Array.BYTES_PER_ELEMENT, // offset
-                    (faceRange[1] - faceRange[0] + 1) * (vertexPerFeature * vertexAttributes.position.valuesPerElement) // count
-                );
-
-                let minHeight = featureVertices.filter( function(coordinate, index) {
-                    return (index + 1) % 3 == 0;
-                }).reduce( function(accumulator, currentValue) {
-                    return Math.min(accumulator, currentValue);
-                });
-
-                offsetVertices(featureVertices, node.mbs[0], node.mbs[1], -minHeight); // move each vertices to right coordinates
-
-                let cartesianPositions = Cesium.Cartesian3.fromDegreesArrayHeights(featureVertices);
-                let positions = new Float64Array(featureVertices.length);
-                
-                cartesianToTypedArray(cartesianPositions, positions);
-
-                let normals = new Float32Array(
-                    geometryBuffer,
-                    vertexAttributes.normal.byteOffset + faceRange[0] * (vertexPerFeature * vertexAttributes.normal.valuesPerElement) * Float32Array.BYTES_PER_ELEMENT,
-                    (faceRange[1] - faceRange[0] + 1) * (vertexPerFeature * vertexAttributes.normal.valuesPerElement)
-                );
-
-                let geometry = new Cesium.Geometry({
-                    attributes: {
-                        position: new Cesium.GeometryAttribute({
-                            componentDatatype: Cesium.ComponentDatatype.DOUBLE,
-                            componentsPerAttribute: 3,
-                            values: positions
-                        }),
-                        normal: new Cesium.GeometryAttribute({
-                            componentDatatype: Cesium.ComponentDatatype.FLOAT,
-                            componentsPerAttribute: 3,
-                            values: normals
-                        })
-                    },
-                    primitiveType: Cesium.PrimitiveType.TRIANGLES,
-                    boundingSphere: Cesium.BoundingSphere.fromVertices(positions)
-                });
-
-                var instance = new Cesium.GeometryInstance({
-                    geometry: geometry,
-                    attributes: {
-                        color: Cesium.ColorGeometryInstanceAttribute.fromColor(color)
-                    },
-                    show: new Cesium.ShowGeometryInstanceAttribute(true),
-                    id: feature.id
-                });
-
-                instances.push(instance);
-            }
-
-            var primitive = viewer.scene.primitives.add(new Cesium.Primitive({
-                geometryInstances: instances,
-                appearance: new Cesium.PerInstanceColorAppearance({
-                    closed: true,
-                    translucent: false
-                })
-            }));
-            primitive.id = node.id;
-            primitive.level = node.level;
-
-            showingBuildings.push(primitive);
+            url: layerUrl + '/nodes/' + node.id + '/' + node.featureData[0].href,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (featureData) {
+    
+            $.ajax({
+                url: layerUrl + '/nodes/' + node.id + '/' + node.geometryData[0].href,
+                xhrFields: {
+                    responseType: "arraybuffer"
+                },
+                processData: false
+            }).done(function (geometryBuffer) {
+    
+                let features = featureData.featureData;
+                let geometryData = featureData.geometryData[0];
+                let vertexAttributes = geometryData.params.vertexAttributes;
+                let instances = new Array();
+    
+                let vertexPerFeature = 3;
+                if (geometryData.params.type == 'triangles') {
+                    vertexPerFeature = 3;
+                } else if (geometryData.params.type == 'lines') {
+                    vertexPerFeature = 2;
+                } else if (geometryData.params.type == 'points') {
+                    vertexPerFeature = 1;
+                }
+    
+                for (let i = 0; i < features.length; i++) {
+                    let feature = features[i];
+                    let faceRange = feature.geometries[0].params.faceRange; // faceRange is the index of faces(triangles): [first, last]
+                    let featureVertices = new Float32Array(
+                        geometryBuffer,
+                        vertexAttributes.position.byteOffset + faceRange[0] * (vertexPerFeature * vertexAttributes.position.valuesPerElement) * Float32Array.BYTES_PER_ELEMENT, // offset
+                        (faceRange[1] - faceRange[0] + 1) * (vertexPerFeature * vertexAttributes.position.valuesPerElement) // count
+                    );
+    
+                    let minHeight = featureVertices.filter( function(coordinate, index) {
+                        return (index + 1) % 3 == 0;
+                    }).reduce( function(accumulator, currentValue) {
+                        return Math.min(accumulator, currentValue);
+                    });
+    
+                    offsetVertices(featureVertices, node.mbs[0], node.mbs[1], -minHeight); // move each vertices to right coordinates
+    
+                    let cartesianPositions = Cesium.Cartesian3.fromDegreesArrayHeights(featureVertices);
+                    let positions = new Float64Array(featureVertices.length);
+                    
+                    cartesianToTypedArray(cartesianPositions, positions);
+    
+                    let normals = new Float32Array(
+                        geometryBuffer,
+                        vertexAttributes.normal.byteOffset + faceRange[0] * (vertexPerFeature * vertexAttributes.normal.valuesPerElement) * Float32Array.BYTES_PER_ELEMENT,
+                        (faceRange[1] - faceRange[0] + 1) * (vertexPerFeature * vertexAttributes.normal.valuesPerElement)
+                    );
+    
+                    let geometry = new Cesium.Geometry({
+                        attributes: {
+                            position: new Cesium.GeometryAttribute({
+                                componentDatatype: Cesium.ComponentDatatype.DOUBLE,
+                                componentsPerAttribute: 3,
+                                values: positions
+                            }),
+                            normal: new Cesium.GeometryAttribute({
+                                componentDatatype: Cesium.ComponentDatatype.FLOAT,
+                                componentsPerAttribute: 3,
+                                values: normals
+                            })
+                        },
+                        primitiveType: Cesium.PrimitiveType.TRIANGLES,
+                        boundingSphere: Cesium.BoundingSphere.fromVertices(positions)
+                    });
+    
+                    var instance = new Cesium.GeometryInstance({
+                        geometry: geometry,
+                        attributes: {
+                            color: Cesium.ColorGeometryInstanceAttribute.fromColor(color)
+                        },
+                        show: new Cesium.ShowGeometryInstanceAttribute(true),
+                        id: feature.id
+                    });
+    
+                    instances.push(instance);
+                }
+    
+                var primitive = viewer.scene.primitives.add(new Cesium.Primitive({
+                    geometryInstances: instances,
+                    appearance: new Cesium.PerInstanceColorAppearance({
+                        closed: true,
+                        translucent: false
+                    })
+                }));
+                primitive.id = node.id;
+                primitive.level = node.level;
+    
+                showingBuildings.push(primitive);
+            });
         });
-    });
+    }
 }
 
 function removeAllMBS() {
